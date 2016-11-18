bd8ed61dfdc0c58a2a72534be671bbd4cdabf981
Andy Zhu <crzyazn888@gmail.com>
2016-11-17 21:35:22 -0500

    Added Step 4

--- app/controllers/ActivityControllers.js
+++ app/controllers/ActivityControllers.js
@@ -253,4 +253,81 @@ angular.module('InfoTimeline')
                 $mdDialog.hide();
             };
         }
+    }])
+    .controller('ActivityStep4Controller', ['$scope', '$routeParams', 'configService', 'dbService', 'activityService', '$mdDialog', '$location', function($scope, $routeParams, configService, dbService, activityService, $mdDialog, $location) {
+        function init() {
+            $scope.routeParams = $routeParams;
+            configService.loadActivity($scope.routeParams.activityFolder).then(function(response) {
+                $scope.activity = response;
+            });
+            dbService.saveState($scope.routeParams.id, 2, $scope.routeParams.activityFolder);
+            dbService.loadData($scope.routeParams.id, 1, 1).then(function(response) {
+                $scope.sources = response;
+            });
+            $scope.helpToggle();
+        }
+
+        /**
+        * Open Instructions Dialog
+        *
+        * @method helpToggle
+        **/
+        $scope.helpToggle = function(ev) {
+            $mdDialog.show({
+                locals:{config: $scope.config},
+                controller: InstructionsDialogController,
+                templateUrl: '/views/step4/instructions.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: false // Only for -xs, -sm breakpoints.
+            });
+        };
+
+        //Instructions Dialog controller
+        function InstructionsDialogController($scope, $mdDialog, config) {
+            $scope.config = config;
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+        }
+
+        init();
+
+        $scope.actionsState = true;
+
+        /**
+        * Save data to database and advance state
+        *
+        * @method saveData
+        **/
+        $scope.saveData = function() {
+            var url = "/activity/"+$scope.routeParams.activityFolder+"/4/"+$scope.routeParams.id;
+            $location.path(url);
+        };
+
+        /**
+        * Display Hint Dialog
+        *
+        * @method showHint
+        **/
+        $scope.showRationale = function(sourceIndex, ev) {
+            $mdDialog.show({
+                locals:{source: $scope.sources[sourceIndex]},
+                controller: RationaleDialogController,
+                templateUrl: '/views/step4/rationale.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: true // Only for -xs, -sm breakpoints.
+            });
+        };
+
+        //Hint Dialog controller
+        function RationaleDialogController($scope, $mdDialog, source) {
+            $scope.source = source;
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+        }
     }]);
